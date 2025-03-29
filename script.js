c616f2421ec6e4e1bdc46c1b53e46a0474ac6ef7
AAMIR TECHNIC <94229247+aamirtechnic@users.noreply.github.com>
2025-03-29 16:51:28 +0300

    Update script.js
    حذف تعليمات

--- script.js
+++ script.js
@@ -238,18 +238,6 @@ function shareImage() {
       document.querySelector(".mobile-hint").style.display = "block";
     });
   });
-// تفعيل زر تعليمات الاستخدام بعد تحميل الصفحة بالكامل
-document.addEventListener("DOMContentLoaded", function() {
-  
-  // فتح نافذة تعليمات الاستخدام
-  document.getElementById('help-btn').onclick = function() {
-    document.getElementById('help-modal').style.display = 'flex';
-  };
-
-  // إغلاق نافذة تعليمات الاستخدام
-  document.getElementById('close-help-btn').onclick = function() {
-    document.getElementById('help-modal').style.display = 'none';
-  };
 
 });
 
